cb1bf66aedc5f844dede0fe0e38afa76cb398920
Chris Geiser <chris@wondertwin.com>
2024-04-10 15:22:36 -0400

    write full league

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,19 @@ from espn_api.baseball import League
 # Call ESPN API
 league = League(
     **st.secrets.espn_credentials,
-    debug=True, #debug mode
+#    debug=True, #debug mode
 )
 
 # Get a list of columns for our dataframe
-#df_columns = list(league.teams[0].__dict__.keys())
+df_columns = list(league.teams[0].__dict__.keys())
 
 # Remove roster from list of columns
-#df_columns.remove('roster')
+df_columns.remove('roster')
 
 # Append teams to dataframe
-#for d in range(len(league.teams)):
-#    team_df = pd.DataFrame(league.teams[d].__dict__, columns=df_columns)
-#    league_data = league_data.append(team_df)
+for d in range(len(league.teams)):
+    team_df = pd.DataFrame(league.teams[d].__dict__, columns=df_columns)
+    league_data = league_data.append(team_df)
 
 # change the index to 'week'
 #league_data.index.names = ['week']
@@ -28,4 +28,4 @@ league = League(
 # add +1 to every week
 #league_data['week'] = league_data['week'].apply(lambda x: x + 1)
 
-st.write(league.teams[-1].__dict__)
+st.dataframe(league_data)
