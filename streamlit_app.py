0ef54905bc74effc433ccc70e5de587fe0c78a2a
Chris Geiser <chris@wondertwin.com>
2024-04-12 12:45:08 -0400

    try json

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,29 +8,46 @@ league = League(
    debug=False, #debug mode
 )
 
+team_data = json_normalize(league)
+
+
+
+# Repeat concatenation into a DataFrame
+#lst = []
+
+#for new_row in items_generation_logic:
+#    lst.append(new_row)
+
+# create extension
+# df_extended = pd.DataFrame(lst, columns=['A', 'B', 'C'])
+# or columns=df.columns if identical columns
+
+# concatenate to original
+# out = pd.concat([df, df_extended])
+
 # Create Teams DataFrame
-team_data = pd.DataFrame()
+# team_data = pd.DataFrame()
 
 # Append teams to DataFrame
-for d in range(len(league.teams)):
-    team_df = league.teams[d].__dict__
-    team_data = team_data.append(team_df, ignore_index=True)
+# for d in range(len(league.teams)):
+#    team_df = league.teams[d].__dict__
+#    team_data = team_data.append(team_df, ignore_index=True)
 
 # Create Roster DataFrame
-roster_data = team_data[['team_id','roster']].copy()
+# roster_data = team_data[['team_id','roster']].copy()
 
 # Create Owners DataFrame
-owner_data = team_data[['team_id','owners']].copy()
+# owner_data = team_data[['team_id','owners']].copy()
 
 # Drop unecessary columns
-team_data = team_data.drop(['division_id','division_name','wins','losses','ties','logo_url','final_standing','schedule','owners', 'roster'], axis=1)
+# team_data = team_data.drop(['division_id','division_name','wins','losses','ties','logo_url','final_standing','schedule','owners', 'roster'], axis=1)
 
 #st.dataframe(league_data)
 st.header("League Teams")
 st.write(team_data)
 
-st.header("Owner Details")
-st.write(owner_data)
+# st.header("Owner Details")
+# st.write(owner_data)
 
-st.header("Roster Details")
-st.write(roster_data)
+# st.header("Roster Details")
+# st.write(roster_data)
